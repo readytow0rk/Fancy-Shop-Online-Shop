eae868399bc56cbad51384023b6033d6ba15fcd0
Nikita <nmal4ik@gmail.com>
2023-05-17 18:56:39 -0700

    add media position for the shop

--- bigshop/settings.py
+++ bigshop/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -96,7 +97,7 @@ WSGI_APPLICATION = 'bigshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
