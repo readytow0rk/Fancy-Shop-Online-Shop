0b8d0b6cd74cf48ef2e24b35f9df9ba3c31c6bc2
Nikita <nmal4ik@gmail.com>
2023-05-20 13:05:01 -0700

    finish with the product form, view, deleting

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -125,6 +127,9 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': dj_database_url.parse('postgres://zfccqbnk:WRrZzSgtoHLvSkz35-DRniQceZ0zJd-5@mel.db.elephantsql.com/zfccqbnk')
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
