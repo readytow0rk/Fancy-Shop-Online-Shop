ffa1a689c8fefe6d9547e2aa0db47698b54e5f3b
readytow0rk <>
2023-05-23 11:40:55 +0000

    fix the settings.py success

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
