1aa9ba6f746f530f1802f2ab86beb990c5e60ca3
readytow0rk <>
2023-05-25 21:38:30 +0000

    addada

---
+++ env.py
@@ -0,0 +1,25 @@
+import os
+
+os.environ["SECRET_KEY"] = "867jhgfd51N9V1tBF5zOyFcJlBjZWAVrdixyYFGI4INkufX3daLVA0JAZLwXGxfv9qtO2zVSK00wXwlZYrQ"
+
+os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51N9V1tBF5zOyFcJlRxjOy4I6h6ic3V67JmFiH8OQVnxNgUcD4hKqGtJl1lx2tnXCZfvDapKY2V5LFgNStoCSmAlF00vjMFFg5U"
+
+os.environ["STRIPE_SECRET_KEY"] = "sk_test_51N9V1tBF5zOyFcJlBUMAtd7VLy5sKORKUa4XwnNpa4ZWAVrdixyYFGI4INkufX3av4XC0JAZLwXGxfv9qtO2zVSK00wXwlZYrQ"
+
+os.environ["STRIPE_WH_SECRET"] = "whsec_v1IvKr29IFxWn1zM1yEO8H5IyRgWlMfz"
+
+os.environ["DATABASE_URL"] = "postgres://zfccqbnk:WRrZzSgtoHLvSkz35-DRniQceZ0zJd-5@mel.db.elephantsql.com/zfccqbnk"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+os.environ["DEVELOPMENT"] = "1"
+
+os.environ["AWS_ACCESS_KEY_ID"] = "AKIA5XISTJE37UEMC3HR"
+
+os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
+
+os.environ["USE_AWS"] = "True"
+
+# os.environ["EMAIL_HOST_USER"] = ""
+
+# os.environ["EMAIL_HOST_PASS"] = ""
